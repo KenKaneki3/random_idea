d08deda4b69284e091606382997923f7a6429ebf
Naman Patel <namanp59@gmail.com>
2023-08-16 21:27:17 +0530

    Third_commit

--- idea_1/lib/main.dart
+++ idea_1/lib/main.dart
@@ -1,9 +1,12 @@
+// ignore_for_file: library_private_types_in_public_api, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
+      theme: ThemeData(fontFamily: 'Roboto'),
     ));
 
 class HomePage extends StatefulWidget {
@@ -50,7 +53,7 @@ class _HomePageState extends State<HomePage> {
                   ),
                   Text(
                     'Inspiration',
-                    style: TextStyle(color: Colors.black87, fontSize: 40),
+                    style: TextStyle(color: Colors.black87, fontSize: 40, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(
                     height: 20,
@@ -84,20 +87,64 @@ class _HomePageState extends State<HomePage> {
             padding: EdgeInsets.symmetric(horizontal: 20.0),
             child: Text(
               'Promo Today',
-              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
           ),
           SizedBox(
             height: 15.0,
           ),
-          Container(
-            height: 200,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: <Widget>[
-                promoCard(''),
-              
-              ],
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 15),
+            child: Container(
+              height: 200,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  promoCard('resource/img/cherry.jpg'),
+                  promoCard('resource/img/ice.jpg'),
+                  promoCard('resource/img/dou.jpg'),
+                  promoCard('resource/img/light.jpg')
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Container(
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('resource/img/gra.jpg')),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient:
+                      LinearGradient(
+                        begin: Alignment.bottomRight, 
+                        stops: [0.4,0.7], colors: [
+                    Colors.black.withOpacity(0.65),
+                    Colors.black.withOpacity(0.2),
+                  ]),
+                ),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Text(
+                    'Best Design',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 244, 242, 242),
+                        fontSize: 25),
+                  ),),
+                ),
+              ),
             ),
           ),
         ],
@@ -105,16 +152,28 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-Widget promoCard(image) {
+  Widget promoCard(image) {
     return AspectRatio(
-      aspectRatio: 2 / 3,
+      aspectRatio: 2.9 / 3,
       child: Container(
+        margin: EdgeInsets.only(right: 15),
         decoration: BoxDecoration(
-          color: Colors.orange,
+          color: Colors.blueAccent.withOpacity(0.5),
           borderRadius: BorderRadius.circular(20),
-          image:
-              DecorationImage(
-                image: AssetImage('assets/images/one.jpg')
+          image: DecorationImage(
+            fit: BoxFit.cover, 
+            image: AssetImage(image)),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: LinearGradient
+            (begin: Alignment.bottomRight, 
+            stops: [0.1,0.5], 
+            colors: [
+              Colors.black.withOpacity(0.5),
+              Colors.black.withOpacity(0.1),
+            ]),
           ),
         ),
       ),
